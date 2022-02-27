22a88308765302c547031d2169eca9018dcbf549
goldenglorys <olusolagloryolamide@gmail.com>
2022-02-27 14:49:47 +0100

    feat: add swagger documentation notion to api

--- app/Http/Controllers/API/v1/WelcomeController.php
+++ app/Http/Controllers/API/v1/WelcomeController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class WelcomeController extends Controller
 {
+    /**
+     * @OA\Get(
+     * path="/api/v1/",
+     * summary="Welcome message",
+     * description="Get welcome message",
+     * operationId="welcome",
+     * tags={"welcome"},
+     * @OA\Response(
+     *    response=200,
+     *    description="Success",
+     *     ),
+     * )
+     */
+
     public function index()
     {
         $response = [
@@ -14,6 +28,6 @@ class WelcomeController extends Controller
             'message' => "Welcome to the Garden of Eden. A new day, another opportunity to be a step closer to 'Paradise'!",
         ];
 
-        return response()->json($response, 200);
+        return response()->json($response, Response::HTTP_OK);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="Eden Life",
+ *    version="1.0.0",
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
